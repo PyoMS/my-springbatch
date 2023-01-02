a99d4ef32c95a3a2b01b1b11b5b2a40167baccbb
PMS <ms147850@gmail.com>
2023-01-02 22:30:53 +0900

    Initial commit

---
+++ src/main/java/io/springbatch/springbatchlecture/MySpringBatchApplication.java
@@ -0,0 +1,13 @@
+package io.springbatch.springbatchlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBatchApplication.class, args);
+    }
+
+}
